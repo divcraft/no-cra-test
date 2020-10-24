34d0c512afe86f1bce12d421a63ad1cf6abaf0ed
divcraft <t.ogonowski@wp.pl>
2020-10-24 15:38:23 +0200

    lint test6

--- __server/database/index.js
+++ __server/database/index.js
@@ -1,11 +1,15 @@
+const mongoose = require('mongoose');
+
 const database = () => {
-  const mongoose = require('mongoose');
-  mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error'));
   db.once('open', () => {
-    console.log('here\'s mongoose, we\'re connected!');
+    console.log("here's mongoose, we're connected!");
   });
 };
 
